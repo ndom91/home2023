be9363763022793fb512624e37826ed09c426698
ndom91 <yo@ndo.dev>
2023-08-15 22:27:33 +0200

    feat: add all existing blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ndo.dev',
   integrations: [mdx(), sitemap(), react(), tailwind()]
 });
