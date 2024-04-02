fccbbdd317fbe688ea41931a448776214126e0b3
ndom91 <yo@ndo.dev>
2024-04-02 18:07:57 +0200

    fix: add postcss-nesting and open-props + tailwind config updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,24 @@
+import { StaticShadows } from "open-props/src/shadows"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         humane: ["HumaneBlack"],
         humaneThin: ["HumaneThin"],
-        humaneLight: ["HumaneLight"]
-      }
+        humaneLight: ["HumaneLight"],
+      },
+    },
+    boxShadow: {
+      xs: StaticShadows["--shadow-1"],
+      sm: StaticShadows["--shadow-2"],
+      md: StaticShadows["--shadow-3"],
+      lg: StaticShadows["--shadow-4"],
+      xl: StaticShadows["--shadow-5"],
+      "2xl": StaticShadows["--shadow-6"],
     },
   },
   plugins: [],
